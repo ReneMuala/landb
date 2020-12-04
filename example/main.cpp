89a63336e7de1dbfb102834c1a908dd139e3e803
ReneMuala <renemuala@icloud.com>
2020-12-04 15:54:44 +0200

    Added func "bool db::is_connected()"

--- example/main.cpp
+++ example/main.cpp
@@ -13,14 +13,20 @@ int main(){
 	//declares a string array inside of the "food" class
 	std::string fruits[] = {"Mango", "Banana", "Apple"};
 	database.set_string_array("food.fruits", fruits, 3);
-	
+
 	// prints the 1st member of our string array
-	std::cout << "string: " << database.get_string("food.fruits[0]") << std::endl;
+	std::cout << "i love " << database.get_string("food.fruits[0]") + 's' << std::endl;
+
+	//changes the 1st member of our string array
+	database.set_string("food.fruits[0]", "Avocado");
+
+        // prints the 1st member of our string array again
+        std::cout << "i also love " << database.get_string("food.fruits[0]") + 's' << std::endl;
 
 	// connects the database to the file "example.ldb"
         database.connect("example.ldb");
 
-	// if ou database is connected, push data to it
+	// if ou database is connected, push data to it (generate offline variables)
 	if(database.is_connected())
 		database.push();
 
